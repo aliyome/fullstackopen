9e59a7fd51fb56740072a68d8eb0ff2f5a139e1c
aliyome <aliyome@gmail.com>
2019-09-19 04:02:00 +0900

    refactor(part1): 高階関数を使うことで冗長な記述をなくした

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 const App = props => {
   const [counter, setCounter] = useState(0);
-  const setToValue = value => setCounter(value);
+  const setToValue = value => () => setCounter(value);
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setToValue(counter + 1)}>plus</button>
-      <button onClick={() => setToValue(0)}>zero</button>
+      <button onClick={setToValue(counter + 1)}>plus</button>
+      <button onClick={setToValue(0)}>zero</button>
     </div>
   );
 };
