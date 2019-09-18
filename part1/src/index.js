e5ba29fa4d0f2ef4ce4c7b14a250903f4fb1d32a
aliyome <aliyome@gmail.com>
2019-09-19 00:49:36 +0900

    feat(part1): オブジェクトの分解を使った引数の受け取り

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = props => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - age;
+  };
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>So you ware probably born in {bornYear()}</p>
     </div>
   );
 };
 
-const Footer = () => {
-  return <div>footer</div>;
-};
-
 const App = () => {
+  const name = 'Peter';
+  const age = 10;
+
   return (
-    <>
+    <div>
       <h1>Greetings</h1>
-      <Hello name="George" />
-      <Hello name="Daisy" />
-      <Footer />
-    </>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   );
 };
 
