88b20250065f414e1b83971b35ae3cb3688d9478
aliyome <aliyome@gmail.com>
2019-09-19 00:00:55 +0900

    feat(part1): 演習問題Step1

---
+++ part1-ex/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = props => <h1>{props.course}</h1>;
+const Content = props => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+);
+const Total = props => <p>Number of exercises {props.num}</p>;
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises1={exercises1} />
+      <Content part={part2} exercises2={exercises2} />
+      <Content part={part3} exercises3={exercises3} />
+      <Total num={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
