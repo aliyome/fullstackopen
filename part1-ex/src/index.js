5eb2bdb850c1827ee51b1dfa1edb6709b066c013
aliyome <aliyome@gmail.com>
2019-09-19 00:25:27 +0900

    refactor(part1): 演習問題Step3 定数値ではなくオブジェクトを渡す

--- part1-ex/src/index.js
+++ part1-ex/src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 const Header = props => <h1>{props.course}</h1>;
 const Part = props => (
   <p>
-    {props.part} {props.exercises}
+    {props.part.name} {props.part.exercises}
   </p>
 );
 const Content = props => (
   <div>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    <Part part={props.part1} />
+    <Part part={props.part2} />
+    <Part part={props.part3} />
   </div>
 );
 
@@ -19,25 +19,24 @@ const Total = props => <p>Number of exercises {props.num}</p>;
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total num={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total num={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
