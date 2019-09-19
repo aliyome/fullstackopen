eb0354220c763db2dcc39a8019f05a70378fed14
aliyome <aliyome@gmail.com>
2019-09-19 15:14:46 +0900

    feat(part2): ノートの追加機能を実装

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './Note';
 
 const App = props => {
-  const { notes } = props;
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
 
   const rows = notes.map(note => <Note key={note.id} note={note} />);
+
+  const addNote = event => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = event => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>{rows}</ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
