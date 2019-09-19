c61919673c53ad5234734756994e92b703bedb2e
aliyome <aliyome@gmail.com>
2019-09-19 17:41:25 +0900

    feat(part2): ノート追加時にDBへの登録を行う

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -29,11 +29,12 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    axios.post('http://localhost:3001/notes', noteObject).then(response => {
+      setNotes(notes.concat(response.data));
+      setNewNote('');
+    });
   };
 
   const handleNoteChange = event => {
