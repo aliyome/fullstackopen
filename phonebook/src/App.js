cfc8c395361eda5f8490543b9b748db77e21bc1c
aliyome <aliyome@gmail.com>
2019-09-19 18:56:58 +0900

    refactor(part2): 電話帳のAppコンポーネントからaxiosへの依存を別ファイルに切り出し

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-import axios from 'axios';
+import personsService from './services/persons';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,9 +11,7 @@ const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(x => {
-      setPersons(x.data);
-    });
+    personsService.getAll().then(returnedPerson => setPersons(returnedPerson));
   }, []);
 
   const filteredPersons = persons.filter(x => x.name.toLowerCase().includes(filter.toLowerCase()));
@@ -25,10 +23,10 @@ const App = () => {
       return;
     }
 
-    const personObject = { id: persons.length + 1, name: newName, number: newNumber };
+    const personObject = { name: newName, number: newNumber };
 
-    axios.post('http://localhost:3001/persons', personObject).then(res => {
-      setPersons(persons.concat(personObject));
+    personsService.create(personObject).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson));
       setNewName('');
       setNewNumber('');
     });
---
+++ phonebook/src/services/persons.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/persons';
+
+const getAll = () => {
+  return axios.get(baseUrl).then(x => x.data);
+};
+
+const create = person => {
+  return axios.post(baseUrl, person).then(x => x.data);
+};
+
+export default { getAll, create };
