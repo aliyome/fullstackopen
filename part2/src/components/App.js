a6e5fe69e907f146e9c2df7b7cb513e87b7089ec
aliyome <aliyome@gmail.com>
2019-09-19 18:08:52 +0900

    refactor(part2): Appコンポーネントからaxiosへの依存を除去

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Note from './Note';
 import noteService from '../services/notes';
 
@@ -9,8 +8,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    noteService.getAll().then(response => {
-      setNotes(response.data);
+    noteService.getAll().then(initialNotes => {
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -21,8 +20,8 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id);
     const changedNote = { ...note, important: !note.important };
-    noteService.update(id, changedNote).then(res => {
-      setNotes(notes.map(note => (note.id !== id ? note : res.data)));
+    noteService.update(id, changedNote).then(returnedNote => {
+      setNotes(notes.map(note => (note.id !== id ? note : returnedNote)));
     });
   };
 
@@ -38,8 +37,8 @@ const App = () => {
       important: Math.random() > 0.5,
     };
 
-    noteService.create(noteObject).then(response => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote));
       setNewNote('');
     });
   };
