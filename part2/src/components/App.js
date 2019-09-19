4a4278f6c794ab90a05c91305bb61583220aaaac
aliyome <aliyome@gmail.com>
2019-09-19 17:21:20 +0900

    feat(part2): ノートをjson-severから取得するように変更

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './Note';
 
-const App = props => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('promise fulfilled');
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  console.log('render', notes.length, 'notes');
 
   const notesToShow = showAll ? notes : notes.filter(x => x.important);
 
