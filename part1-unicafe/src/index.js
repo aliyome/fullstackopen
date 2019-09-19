a2625dd9e5a74d0dcb3eae549889289f07e7a05a
aliyome <aliyome@gmail.com>
2019-09-19 12:42:44 +0900

    feat(part1): unicafe step2 統計情報を表示

--- part1-unicafe/src/index.js
+++ part1-unicafe/src/index.js
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -17,6 +21,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average || '-'}</div>
+      <div>positive {positive || '-'} %</div>
     </div>
   );
 };
