69627a6d8877ceb5ad18685f9b179309cdcb0aa8
aliyome <aliyome@gmail.com>
2019-09-19 14:32:05 +0900

    refactor(part2): コースデータをAppコンポーネントからindexに移動

--- part1-ex/src/components/App.js
+++ part1-ex/src/components/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import Course from './Course';
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
-
+const App = ({ course }) => {
   return (
     <div>
       <Course course={course} />
--- part1-ex/src/index.js
+++ part1-ex/src/index.js
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ],
+};
+
+ReactDOM.render(<App course={course} />, document.getElementById('root'));
