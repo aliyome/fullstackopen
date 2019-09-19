b1f1c8eb7b176dc4daf24b0bfda414a0c3ad5f3f
aliyome <aliyome@gmail.com>
2019-09-20 01:51:27 +0900

    feat(part3): 電話帳バックエンドにインフォページを返すapiを実装

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const app = express();
 
-const persons = {
-  persons: [
-    {
-      name: 'Arto Hellas',
-      number: '040-123456',
-      id: 1,
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-5323523',
-      id: 2,
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345',
-      id: 3,
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-      id: 4,
-    },
-  ],
-};
+const persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const message = `<p>Phonebook has info for ${
+    persons.length
+  } people<p><p>${new Date().toString()}</p>`;
+  res.send(message);
 });
 
 const PORT = 3001;
