bdcd44f85d9128561538965b3ba37f1675e43df5
aliyome <aliyome@gmail.com>
2019-09-19 17:50:00 +0900

    feat(part2): ノートの重要度をトグルするボタンを追加

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -21,7 +21,18 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter(x => x.important);
 
-  const rows = notesToShow.map(note => <Note key={note.id} note={note} />);
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    axios.put(url, changedNote).then(res => {
+      setNotes(notes.map(note => (note.id !== id ? note : res.data)));
+    });
+  };
+
+  const rows = notesToShow.map(note => (
+    <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+  ));
 
   const addNote = event => {
     event.preventDefault();
