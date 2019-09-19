b2d48fa236cb6f0be5aeefc26903a267388c1bf2
aliyome <aliyome@gmail.com>
2019-09-19 12:21:57 +0900

    feat(part1): unicafe step1 カウントアップの実装

--- part1-unicafe/src/index.js
+++ part1-unicafe/src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <></>;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
