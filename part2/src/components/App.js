34b865d77d2a442a208c3ebf14b0da42d6e7aada
aliyome <aliyome@gmail.com>
2019-09-19 19:49:10 +0900

    feat(part2): ノートの重要度を変更した時にアラートではなくメッセージを表示する

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Note from './Note';
 import noteService from '../services/notes';
+import Notification from './Notification';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => {
@@ -26,7 +28,11 @@ const App = () => {
         setNotes(notes.map(note => (note.id !== id ? note : returnedNote)));
       })
       .catch(err => {
-        alert(`the note '${note.content}' was already deleted from server.`);
+        setErrorMessage(`Note '${note.content}' was already removed from server.`);
+
+        setTimeout(() => {
+          setErrorMessage(null, 5000);
+        });
         setNotes(notes.filter(x => x.id !== id));
       });
   };
@@ -57,6 +63,9 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+
+      <Notification message={errorMessage} />
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
       </div>
